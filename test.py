c6b9baccce430bfbfece5061fcae3a401e82a717
Sunil <sunhick@gmail.com>
2016-09-01 16:45:24 -0600

    added more test

--- test.py
+++ test.py
@@ -10,8 +10,12 @@ class calc(unittest.TestCase):
     def tearDown(self):
         pass
     
-    def test_add(self):
-        self.assertTrue(10, Add(3, 7))
+    def test_add_true(self):
+        self.assertEqual(10, Add(3, 7))
+
+    def test_add_false(self):
+        self.assertNotEqual(100, Add(3, 7))
+
 
 if __name__ == "__main__":
     unittest.main()
